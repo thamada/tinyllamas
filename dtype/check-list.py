4c6630e733f8b53a20baaff4a244053bb16b7783
thamada <thamada@users.noreply.github.com>
2024-08-12 17:28:43 +0900

    update

--- dtype/check-list.py
+++ dtype/check-list.py
@@ -2,40 +2,28 @@
 import time
 import random
 import sys
+from memory_profiler  import profile
 
-def getSize(array_byte):
-    mb =  array_byte / (1024.*1024.)
-    print ("%d Mbyte" %  mb, flush=True) 
 
 def init():
-    array_byte = 0
 
     random.seed(time.time())
-
     n_list = 1024 * 1024 * 1024
     z = [0.] * n_list
 
-    array_byte += sys.getsizeof(z)
-
-    print ('start for')
     for i in range(n_list):
 
         if 0 == i % 100000:
             print('|', end='', flush=True)
 
-        if 0 == i % 10000000:
-            getSize(array_byte)
-
         x = random.random()
-        array_byte += sys.getsizeof(x)
         z[i] = x
-
 
     return z
 
 
-if __name__ == "__main__":
-
+@profile
+def main():
     v = init()
 
     for i in range(len(v)-10, len(v)):
@@ -43,5 +31,11 @@ if __name__ == "__main__":
 
     print ('-'*100, flush=True)
 
+'''
     while True:
-        time.sleep(100000)
+        time.sleep(120)
+'''
+
+if __name__ == "__main__":
+
+    main()
